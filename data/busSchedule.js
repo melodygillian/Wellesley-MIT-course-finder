fde4e5c8b12a0e09905f245d69714d7c943c52c8
melodygillian <155018755+melodygillian@users.noreply.github.com>
2026-01-13 15:27:16 -0500

    Add Wellesley-MIT Exchange Bus Schedule
    This file contains the Wellesley-MIT Exchange Bus Schedule for Monday to Friday, detailing bus A's stop times throughout the day.

---
+++ data/busSchedule.js
@@ -0,0 +1,43 @@
+// Wellesley-MIT Exchange Bus Schedule (Monday-Friday)
+const BUS_SCHEDULE = [
+  {bus: "A", stopTimes: {
+    "Chapel_out":"9:00 am", "Alumnae_out":"9:05 am",
+    "Harvard Sq.":"9:45 am", "77 Mass Ave.":"9:55 am", "Marlboro Market":"10:00 am",
+    "Alumnae_in":"10:30 am", "Chapel_in":"10:35 am"
+  }},
+  {bus: "A", stopTimes: {
+    "Chapel_out":"11:00 am", "Alumnae_out":"11:05 am",
+    "Harvard Sq.":"11:45 am", "77 Mass Ave.":"11:55 am", "Marlboro Market":"12:00 pm",
+    "Alumnae_in":"12:30 pm", "Chapel_in":"12:35 pm"
+  }},
+  {bus: "A", stopTimes: {
+    "Chapel_out":"1:00 pm", "Alumnae_out":"1:05 pm",
+    "Harvard Sq.":"1:45 pm", "77 Mass Ave.":"1:55 pm", "Marlboro Market":"2:00 pm",
+    "Alumnae_in":"2:30 pm", "Chapel_in":"2:35 pm"
+  }},
+  {bus: "A", stopTimes: {
+    "Chapel_out":"3:00 pm", "Alumnae_out":"3:05 pm",
+    "Harvard Sq.":"3:45 pm", "77 Mass Ave.":"3:55 pm", "Marlboro Market":"4:00 pm",
+    "Alumnae_in":"4:30 pm", "Chapel_in":"4:35 pm"
+  }},
+  {bus: "A", stopTimes: {
+    "Chapel_out":"5:00 pm", "Alumnae_out":"5:05 pm",
+    "Harvard Sq.":"5:45 pm", "77 Mass Ave.":"5:55 pm", "Marlboro Market":"6:00 pm",
+    "Alumnae_in":"6:30 pm", "Chapel_in":"6:35 pm"
+  }},
+  {bus: "A", stopTimes: {
+    "Chapel_out":"7:00 pm", "Alumnae_out":"7:05 pm",
+    "Harvard Sq.":"7:45 pm", "77 Mass Ave.":"7:55 pm", "Marlboro Market":"8:00 pm",
+    "Alumnae_in":"8:30 pm", "Chapel_in":"8:35 pm"
+  }},
+  {bus: "A", stopTimes: {
+    "Chapel_out":"9:00 pm", "Alumnae_out":"9:05 pm",
+    "Harvard Sq.":"9:45 pm", "77 Mass Ave.":"9:55 pm", "Marlboro Market":"10:00 pm",
+    "Alumnae_in":"10:30 pm", "Chapel_in":"10:35 pm"
+  }},
+  {bus: "A", stopTimes: {
+    "Chapel_out":"11:00 pm", "Alumnae_out":"11:05 pm",
+    "Harvard Sq.":"11:45 pm", "77 Mass Ave.":"11:55 pm", "Marlboro Market":"12:00 am",
+    "Alumnae_in":"12:30 am", "Chapel_in":"12:35 am"
+  }},
+];
